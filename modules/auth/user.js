f59c26bbffe86c8d05558de6b5c8bbd826e56281
hihi-haha-hoho <dinhquangdung1011@gmail.com>
2021-04-23 20:30:32 +0700

    +role

--- modules/auth/user.js
+++ modules/auth/user.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
 	email: { type: String, required: true },
 	password: { type: String, require: true },
+	role: { type: String, require: true },
 });
 
 module.exports = mongoose.model("user", UserSchema);
